2170eef1362f990e89ab0cd193bf56d80318744b
ConnorMulqueen <connor.mulqueen@gmail.com>
2019-10-30 18:00:21 -0400

    change to FA icons

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,9 +8,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import Avatar from '@material-ui/core/Avatar';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+
 
 const useStyles = makeStyles({
     bigAvatar: {
@@ -21,13 +24,15 @@ const useStyles = makeStyles({
       alignItems: 'center',
       alignSelf: 'center'
     },
+    background: {
+    },
   });
 
 // should be using typography
 const Home: React.FC = () => {
     const classes = useStyles();
     return (
-        <div>
+        <div className={classes.background}>
             <Container>
                 <Grid container justify = "center">
                     <Avatar alt="profile_pic" src={require('./Assets/Images/me2.jpg')} className={classes.bigAvatar} />
@@ -50,7 +55,10 @@ const Home: React.FC = () => {
                 </Typography>
             <List component="nav" aria-label="main mailbox folders">
         <ListItem button>
-          <ListItemText primary="Mechanical Squirrel" secondary='A discord bot for World of Warcraft: Classic memes' />
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src={require('./Assets/Images/me2.jpg')} />
+        </ListItemAvatar>
+          <ListItemText primary="Mechanical Squirrel" secondary='A Discord bot for World of Warcraft: Classic memes (15k+ Users | 500+ Discord Servers)' />
         </ListItem>
         </List>
             </Container>
